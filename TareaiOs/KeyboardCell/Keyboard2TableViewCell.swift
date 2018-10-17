58da4062a940621274fee565fb59b5bf4310d6c9
JoakoDR <joaco_diaz_ramirez@hotmail.com>
2018-10-17 14:35:11 +0200

    ¡ Design !

---
+++ TareaiOs/KeyboardCell/Keyboard2TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Keyboard2TableViewCell.swift
+//  TareaiOs
+//
+//  Created by JOAQUIN DIAZ RAMIREZ on 17/10/18.
+//  Copyright © 2018 JOAQUIN DIAZ RAMIREZ. All rights reserved.
+//
+
+import UIKit
+
+class Keyboard2TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
